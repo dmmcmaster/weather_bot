81ea316636821c71d6d4398de9ad1269c22a24f1
dmmcmaster <dmmcmaster@wpi.edu>
2017-12-14 19:39:24 -0500

    app.json

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const superagent = require('superagent')
 const app = express()
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+//app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(bodyParser.json())
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+app.post('/api', (req, res) => {
+  if (req.body.type === 'new_message') {
+    handleMessage(req.body.orgId, req.body.data)
+  }
+  if (req.body.type === 'button_action') {
+    handleButton(req.body.orgId, req.body.data)
+  }
+  return res.send('ok')
+})
